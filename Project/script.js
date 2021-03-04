2e595410677666aa7f34ee40a2df23cefb037fa4
taliaeduarda <78424901+taliaeduarda@users.noreply.github.com>
2021-03-04 09:54:53 -0300

    All bugs fixed

--- Project/script.js
+++ Project/script.js
@@ -1,51 +1,42 @@
 'use strict'
 
-var addButton = document.getElementById("add");
-var input = document.getElementById("inputValue")
-var ul = document.querySelector("ul");
-var item = document.getElementsByTagName("li");
+let addButton = document.getElementById("add");
+let input = document.getElementById("inputValue");
+let ul = document.querySelector("ul");
+
+function inputLength(){
+	return input.value.length;
+} 
 
 function listLength(){
 	return item.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li"); // creates an element "li"
-	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
-	ul.appendChild(li); //adds li to ul
-	input.value = ""; //Reset text input field
+	let li = document.createElement("li")
+	li.appendChild(document.createTextNode(input.value))
+	ul.appendChild(li)
+	input.value = ""
 
+	// start and delete button
+	let deleteButton = document.createElement("button");
+	deleteButton.appendChild(document.createTextNode("x"));
+	li.appendChild(deleteButton);
+	deleteButton.addEventListener("click", deleteListItem);
 
-	// START ADD DELETE BUTTON
-	let deleteBtn = document.createElement("button");
-	deleteBtn.appendChild(document.createTextNode("X"));
-	li.appendChild(deleteBtn);
-	deleteBtn.addEventListener("click", deleteListItem);
-	// END ADD DELETE BUTTON
-
-
-	//ADD CLASS DELETE (DISPLAY: NONE)
+	// add a delete class
 	function deleteListItem(){
 		li.classList.add("delete")
 	}
-
 }
 
-
 function addListAfterClick(){
-	if (input.length > 0) { //makes sure that an empty input field doesn't create a li
-		createListElement();
+	if (inputLength() > 0) { 
+		createListElement()
 	}
 }
 
-function addListAfterKeypress(event) {
-	if (input.length > 0 && event.which === 13) { //this now looks to see if you hit "enter"/"return"
-		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
-		createListElement();
-	} 
-}
-
-
 addButton.addEventListener("click", addListAfterClick);
 
-input.addEventListener("keypress", addListAfterKeypress);
+
+
